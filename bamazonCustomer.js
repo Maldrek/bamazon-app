96010d44eeb751918f0b22d43d43fc6348c3f6fc
Maldrek <scott.salem5@gmail.com>
2018-04-28 13:53:05 -0500

    updated code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,7 +11,7 @@ let connection = mysql.createConnection({
     user: "root",
 
     // Telling what password (if needed) and database to connect to
-    password: "Magicman1684",
+    password: "",
     database: "bamazon"
 });
 
